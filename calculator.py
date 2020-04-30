425d1295297766da9618cbaaa4f3826cf7dc8163
Eren Kim <erenkim@gmail.com>
2020-04-30 10:03:54 -0400

    Forgot to insert tokenized input. Updated WHILE loop: Return value is printed out.

--- calculator.py
+++ calculator.py
@@ -6,12 +6,15 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 print ("Welcome to the Prefix Calculator!")
 
 # User's input is assigned to input_String as one string,
-input_string = input (print ("Enter your equation:  "))
+input_string = input ("Enter your equation:  ")
+
+#Tokenize input. 
+tokens = input_string.split(' ')
 
 # Assign more readable names to tokenized items. 
-operator = input_string [0]
-num1 = input_string [1]
-num2 = input_string [2] 
+operator = tokens [0]
+num1 = tokens [1]
+num2 = tokens [2] 
 
 # Insert while loop here. Keep asking for equation until user quits. 
 
@@ -19,24 +22,25 @@ num2 = input_string [2]
 while operator != 'q': 
     # Call the function determined by 'operator'. 
     if operator == '+': 
-        add (num1, num2)
+        result = add (num1, num2)
     elif operator == '-':
-        subtract (num1, num2)
+        result = subtract (num1, num2)
     elif operator == '*': 
-        multiply (num1, num2)
+        result = multiply (num1, num2)
     elif operator == '/': 
-        divide (num1, num2)
+        result = divide (num1, num2)
     elif operator == 'square': 
-        square (num1, num2)
+        result = square (num1, num2)
     elif operator == 'cube': 
-        cube (num1, num2)
+        result = cube (num1, num2)
     elif operator == 'pow': 
-        power (num1, num2) 
+        result = power (num1, num2) 
     elif operator == '%': 
-        mod (num1, num2)
+        result = mod (num1, num2)
     else: 
         # Exit WHILE loop if user entered 'q' or something irrelevant. 
         break 
-
+    print (result)
+    
 
 
