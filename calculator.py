7d18c15faf1ca966ec72e65ce41e3c9b76da8aad
Eren Kim <erenkim@gmail.com>
2020-04-30 09:21:31 -0400

    Check if user wants to quit program.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
 """CLI application for a prefix-notation calculator."""
 
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+print ("Welcome to the Prefix Calculator!")
+
+# User's input is assigned to input_String as one string,
+input_string = input (print ("Enter operator followed by two numbers:  "))
+
+# Check if user wants to quit program. 
+if input_string[0] == 'q': 
+    break
+else: 
+    #
 
