5e30f265053cbf6974570373a406cf729f1e4b18
Eren Kim <erenkim@gmail.com>
2020-04-30 09:25:25 -0400

    Assign more readable names to tokenized items in input string.

--- calculator.py
+++ calculator.py
@@ -8,9 +8,15 @@ print ("Welcome to the Prefix Calculator!")
 # User's input is assigned to input_String as one string,
 input_string = input (print ("Enter operator followed by two numbers:  "))
 
+# Assign more readable names to tokenized items. 
+operator = input_string [0]
+num1 = input_string [1]
+num2 = input_string [2] 
+
 # Check if user wants to quit program. 
 if input_string[0] == 'q': 
     break
 else: 
-    #
+    # Look for the right function. 
+    if 
 
